b97bd5e7c74931ff48f32563615b312af0103606
bevinh <admin_bah34@CMFR-143.local>
2017-01-01 22:51:01 -0500

    refactored to disinclude the scraperjs since I can write it anyway

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 /* Content Scraper for Mike's T-shirts, a project by Bevin Hernandez for the Treehouse Full Stack Javascript TechDegree */
 
-var scraperjs = require('scraperjs');
 var mkdirp = require('mkdirp');
 const fs = require('fs');
 var csv = require("fast-csv");
 const url = require('url');
+var request = require('request');
+var cheerio = require('cheerio');
+
 
 //setup the collection arrays
 var arrayOfHrefs = [];
 var path = "data";
+var hrefMike;
+var shirtTitle;
+var shirtImage;
+var shirtPrice;
+var shirtUrl;
+var shirtTime;
 var dataKept = [];
 
 //the mkdirp module makes the folder, but if it already exists, it does nothing
@@ -17,52 +25,50 @@ mkdirp(path, function (err) {
     else console.log('Made data folder');
 });
 
-//scrape the site to determine the subpages
-scraperjs.StaticScraper.create('http://www.shirts4mike.com/shirts.php')
-    .scrape(function($) {
-        return $(".products li a").map(function() {
-            //pull the hrefs
-            return $(this).attr("href");
-        }).get();
-    })
-    .then(function(scrapeArray) {
-        //then set up the sub-array function to scrape the links
-        function scrapeArrayLinks() {
-            //push the hrefs into an array
-            for(i=0;i<scrapeArray.length;i++){
-                var shirtLink = "http://www.shirts4mike.com/";
-                shirtLink += scrapeArray[i];
-                arrayOfHrefs.push(shirtLink);
-            }
-        }
-        //execute the function, to be clear
-        scrapeArrayLinks();
-        //scrape for the shirt details, based on this array
-        shirtScraping();
-    });
-function shirtScraping() {
-    for (i = 0; i < arrayOfHrefs.length; i++) {
-        scraperjs.StaticScraper.create(arrayOfHrefs[i])
-            .scrape(function ($) {
-                return {
-                    title: $(".shirt-details h1").first().contents().filter(function() {
-                    return this.nodeType == 3;
-                }).text(),
-                    price: $(".price").first().text(),
-                    imageURL: $(".shirt-picture span img").first().attr("src"),
-                    url: console.log(i),
-                    time: new Date().toLocaleString()
-                };
-            })
-            .then(function (data, options) {
-                //push each data object into an array
-                dataKept.push(data);
-                //use the csv Populate function to write the file
-                csvPopulate(dataKept);
-            })
-
+//make a request to the main page
+request('http://www.shirts4mike.com/shirts.php', function (error, response, body) {
+    //Check for any errors
+    if(error){
+        return console.log('Oops, something went wrong, looks like an error:', error);
     }
 
+    //Check for the status code
+    if(response.statusCode !== 200){
+        return console.log('Oops, something went wrong, looks like an error:', response.statusCode);
+    }
+    var $ = cheerio.load(body);
+    $('.products li a').each(function(i, element){
+        //console.log($(this).attr("href"))
+        hrefMike = "http://www.shirts4mike.com/" + $(this).attr("href")
+        arrayOfHrefs.push(hrefMike);
+    });
+    for(i=0;i<arrayOfHrefs.length;i++){
+        getShirtData(arrayOfHrefs[i]);
+    }
+});
+
+function getShirtData(shirtLink) {
+    request(shirtLink, function (error, response, body) {
+        //Check for any errors
+        if (error) {
+            return console.log('Oops, something went wrong, looks like an error:', error);
+        }
+
+        //Check for the status code
+        if (response.statusCode !== 200) {
+            return console.log('Oops, something went wrong, looks like an error:', response.statusCode);
+        }
+        var $ = cheerio.load(body);
+        shirtTitle = $('.shirt-details h1').first().contents().filter(function () {
+            return this.nodeType == 3;
+        }).text();
+        shirtPrice = $(".price").first().text();
+        shirtImage = $(".shirt-picture span img").first().attr("src");
+        shirtUrl = shirtLink;
+        shirtTime = new Date().toLocaleString();
+        dataKept.push(shirtTitle, shirtPrice, shirtImage, shirtUrl, shirtTime);
+        csvPopulate(dataKept);
+    })
 }
 
 function csvPopulate(data){
@@ -82,12 +88,9 @@ function csvPopulate(data){
 
     csvStream.pipe(writableStream);
    //Pipe in each data object after iterating through them
-    for(i=0;i<data.length;i++){
-        csvStream.write(data[i]);
-    }
-
+    csvStream.write(data);
     csvStream.end();
 }
 
-
+//TODO: Figure out how to make data maybe not one long horizontal stream lol
 //TODO: Don't forget error handling
